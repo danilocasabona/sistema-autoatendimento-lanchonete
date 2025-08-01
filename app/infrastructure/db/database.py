6dd8abc63554f8745c08cb543c58fce9f1280d7a
Alana Silva <alana_brito@live.com>
2025-08-01 18:45:58 -0300

    correção da falha na comunicação com o banco de dados

--- app/infrastructure/db/database.py
+++ app/infrastructure/db/database.py
@@ -1,9 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-#import os
+import os
+
+DB_USER = os.environ["DB_USER"]
+DB_PASSWORD = os.environ["DB_PASSWORD"]
+DB_HOST = os.environ["DB_HOST"]
+DB_PORT = os.environ["DB_PORT"]
+DB_NAME = os.environ["DB_NAME"]
 
 #DATABASE_URL = os.getenv("database_url")
-DATABASE_URL = "postgresql://postgres:postgres@db:5432/lanchonete"
+#DATABASE_URL = "postgresql://postgres:postgres@db:5432/lanchonete"
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
