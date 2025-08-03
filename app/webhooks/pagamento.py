ac6f3cef43a99869f2260dcd6084010668fa41c0
EstacioDiFabio <estaciodifabio@hotmail.com>
2025-08-03 11:30:23 -0300

    chore: adiciona doc no webhook

--- app/webhooks/pagamento.py
+++ app/webhooks/pagamento.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 
 from app.gateways.pagamento_gateway import PagamentoGateway
 from app.infrastructure.db.database import get_db
-from app.adapters.schemas.pagamento import PagamentoAtualizaWebhookSchema
+from app.adapters.schemas.pagamento import PagamentoAtualizaWebhookSchema, PagamentoResponseSchema
 from app.controllers.pagamento_controller import PagamentoController
 
 router = APIRouter(prefix="/webhook", tags=["webhook"])
@@ -12,7 +12,28 @@ def get_pagamento_gateway(db: Session = Depends(get_db)) -> PagamentoGateway:
     
     return PagamentoGateway(db_session=db)
 
-@router.post("/update-payment")
+@router.post("/update-payment", response_model=PagamentoResponseSchema, responses={
+    404: {
+        "description": "Erro de validação",
+        "content": {
+            "application/json": {
+                "example": {
+                    "message": "Pagamento não encontrado"
+                }
+            }
+        }
+    },
+    400: {
+        "description": "Erro de validação",
+        "content": {
+            "application/json": {
+                "example": {
+                    "message": "Erro de integridade ao atualizar o pagamento"
+                }
+            }
+        }
+    }
+})
 def atualizar_pagamento(pagamento_data: PagamentoAtualizaWebhookSchema, db_session: PagamentoGateway = Depends(get_pagamento_gateway)):
     try:
         
